72bead74fb1552e19642f67da96ab36318a16110
Heart Narongrit <narongrit@3dsinteractive.com>
2022-04-04 18:30:54 +0700

    update download path

--- Package.swift
+++ Package.swift
@@ -14,5 +14,5 @@ let package = Package(
   targets: [
     .binaryTarget(
        name: "PGW",
-       url: "https://github.com/heart/2c2p-swiftpackage/releases/download/4.4.1/PGW4.4.1.xcframework.zip",
+       url: "https://github.com/heart/2c2p-swiftpackage/releases/download/4.4.1/PGW.xcframework.zip",
        checksum: "37aff71cf75a9be22cece3fd0dcfdb7578fca8884b4b0fb0544fd6b538ef11bf") ])
